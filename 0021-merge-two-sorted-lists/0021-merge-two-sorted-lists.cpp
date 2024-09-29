bbee8c701b00c17d539325f9624eec876218067f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-09-30 01:06:25 +0530

    Time: 4 ms (68.63%), Space: 19.7 MB (57.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -20,27 +20,4 @@ public:
         list1->next = mergeTwoLists(list1->next, list2);
         return list1;
     }
-
-    ListNode* mergeTwoLists2(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* tail = dummy;
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val < list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
-        }
-        if (list1 != nullptr) {
-            tail->next = list1;
-        } else {
-            tail->next = list2;
-        }
-        ListNode* mergedList = dummy->next;
-        delete dummy;
-        return mergedList;
-    }
 };
