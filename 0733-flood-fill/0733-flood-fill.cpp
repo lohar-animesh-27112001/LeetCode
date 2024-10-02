df802590f90e3fa0be0767c571072f3a380e7903
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-10-03 00:07:26 +0530

    Time: 4 ms (84.77%), Space: 17.2 MB (50.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,8 +3,31 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int initialColor = image[sr][sc];
         if (initialColor == color) return image;
-        dfs(image, initialColor, sr, sc, color);
+        bfs(image, initialColor, sr, sc, color);
         return image;
+    }
+
+    void bfs(vector<vector<int>>& image, int initialColor, int sr, int sc, int color) {
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        while(!q.empty()) {
+            pair<int, int> p = q.front();
+            q.pop();
+            int first = p.first, second = p.second;
+            image[first][second] = color;
+            if(first + 1 >= 0 && first + 1 < image.size() && image[first + 1][second] == initialColor) {
+                q.push({first + 1, second});
+            }
+            if(first - 1 >= 0 && first - 1 < image.size() && image[first - 1][second] == initialColor) {
+                q.push({first - 1, second});
+            }
+            if(second + 1 >= 0 && second < image[0].size() && image[first][second + 1] == initialColor) {
+                q.push({first, second + 1});
+            }
+            if(second - 1 >= 0 && second < image[0].size() && image[first][second - 1] == initialColor) {
+                q.push({first, second - 1});
+            }
+        }
     }
 
     void dfs(vector<vector<int>>& image, int initialColor, int sr, int sc, int color) {
