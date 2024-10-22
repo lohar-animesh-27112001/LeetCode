4bb36ad6c47cd872714d7e798cdd0946d6bf9e7f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-10-22 10:48:19 +0530

    Time: 7 ms (87.62%), Space: 8.5 MB (25.89%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int i = 0;
-        while(pow(3, i) <= n) {
-            if(pow(3, i) == n) {
+        long long i = 1;
+        while(i <= n) {
+            if(i == n) {
                 return true;
             }
-            i++;
+            i *= 3;
         }
         return false;
     }
