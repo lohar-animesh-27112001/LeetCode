d9776a71371bc567cb5eb92088716ddae7cf0490
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-10-29 15:35:41 +0530

    Time: 0 ms (100%), Space: 10.5 MB (34.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        count_sort(nums);
+        count_sort_all(nums);
     }
 private:
     void count_sort(vector<int> &nums) {
@@ -19,5 +19,26 @@ private:
                 countArray[i]--;
             }
         }
+    }
+    void count_sort_all(vector<int> &nums) {
+        int max = 0;
+        for(int i : nums) {
+            if(i > max) {
+                max = i;
+            }
+        }
+        vector<int> countArray(max + 1, 0);
+        for(int i : nums) {
+            countArray[i] ++;
+        }
+        for(int i = 1; i <= max; i++) {
+            countArray[i] += countArray[i - 1];
+        }
+        vector<int> outputArray(nums.size());
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            outputArray[countArray[nums[i]] - 1] = nums[i];
+            countArray[nums[i]] --;
+        }
+        nums = outputArray;
     }
 };
