ef7e60a4f9a44dee1b978a4d86a8cb030562599b
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-10-31 13:53:30 +0530

    Time: 0 ms (100%), Space: 11.5 MB (22.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -21,29 +21,29 @@ public:
         }
         nums1.resize(m + n);
     }
-    void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0 , j = 0;
-        vector<int> answer;
-        while(m > i && n > j) {
-            if(nums1[i] < nums2[j]) {
-                answer.push_back(nums1[i]);
-                i++;
-            } else {
-                answer.push_back(nums2[j]);
-                j++;
-            }
-        }
-        if(m > i) {
-            while(m > i) {
-                answer.push_back(nums1[i]);
-                i++;
-            }
-        } else {
-            while(n > j) {
-                answer.push_back(nums2[j]);
-                j++;
-            }
-        }
-        nums1 = answer;
-    }
+    // void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     int i = 0 , j = 0;
+    //     vector<int> answer;
+    //     while(m > i && n > j) {
+    //         if(nums1[i] < nums2[j]) {
+    //             answer.push_back(nums1[i]);
+    //             i++;
+    //         } else {
+    //             answer.push_back(nums2[j]);
+    //             j++;
+    //         }
+    //     }
+    //     if(m > i) {
+    //         while(m > i) {
+    //             answer.push_back(nums1[i]);
+    //             i++;
+    //         }
+    //     } else {
+    //         while(n > j) {
+    //             answer.push_back(nums2[j]);
+    //             j++;
+    //         }
+    //     }
+    //     nums1 = answer;
+    // }
 };
