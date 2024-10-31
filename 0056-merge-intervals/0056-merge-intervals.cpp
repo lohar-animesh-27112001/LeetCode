67fe3b939973069bad3242bbc19f7cc5036e67f7
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-10-31 12:52:57 +0530

    Time: 14 ms (12.09%), Space: 25 MB (8.12%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> answer;
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
+        if(n > 0) {
+            answer.push_back(intervals[0]);
+        }
+        int ind = 0;
+        for(int i = 1; i < n; i++) {
+            vector<int> v = intervals[i];
+            vector<int> v_back = answer[ind];
+            if(v[0] <= v_back[1]) {
+                answer[ind][1] = max(v[1], v_back[1]);
+            } else {
+                answer.push_back(v);
+                ind ++;
+            }
+        }
+        return answer;
+    }
+};
