9497ea6d5ec0bc3c37a1b795c972cf6f9ee6e92c
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-11-01 20:19:34 +0530

    Time: 19 ms (89.61%), Space: 42.9 MB (30.23%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n = s.length();
+        if (n <= 2) {
+            return s;
+        }
+        string ans;
+        ans.push_back(s[0]);
+        ans.push_back(s[1]);
+
+        for (int i = 2; i < n; i++) {
+            if (!(s[i] == s[i - 1] && s[i] == s[i - 2])) {
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
