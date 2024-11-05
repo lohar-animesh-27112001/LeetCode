ac6a41354e4510a20f7c0fb6c1f688275c0a4a31
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-11-06 05:18:44 +0530

    Time: 4 ms (71.25%), Space: 11.7 MB (13.74%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
+bool canConstruct(string ransomNote, string magazine) {
+        map<char, int> count;
+        for(char c : magazine) count[c]++;
+        for(char c : ransomNote) if(--count[c] < 0) return false;
+        return true;
+    }
+    bool canConstruct2(string ransomNote, string magazine) {
         vector<int> count(26, 0);
         for(char c : magazine) count[c - 97]++;
         for(char c : ransomNote) if(--count[c - 97] < 0) return false;
