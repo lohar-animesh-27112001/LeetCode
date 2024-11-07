73a381c92786e3752b668cb5bb5e813d681df5a9
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-11-07 09:38:47 +0530

    Time: 40 ms (26.12%), Space: 60.2 MB (74.4%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -5,11 +5,9 @@ public:
         int ans = 0;
         for(int i = 0; i < kMaxBit; i++) {
             int count = 0;
-            for(int candidate : candidates) {
-                if(candidate >> i & 1) {
+            for(int candidate : candidates)
+                if(candidate >> i & 1)
                     count++;
-                }
-            }
             ans = max(ans, count);
         }
         return ans;
