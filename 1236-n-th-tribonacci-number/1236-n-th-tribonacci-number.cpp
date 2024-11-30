156fdecf9d9bd3118865d4057d6b51ab1ae62a5a
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2024-11-30 12:58:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (5.16%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -7,9 +7,9 @@ public:
 // Dynamic programming or memoization
 private:
     int helper(int n, vector<int>& memo) {
+        if (memo[n] != -1) return memo[n];
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        if (memo[n] != -1) return memo[n];
         return memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3,
                                 memo);
     }
