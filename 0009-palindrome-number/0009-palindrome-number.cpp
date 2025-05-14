c339c5c5445f3737fee1112138aaf7b57af23941
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-05-14 17:12:08 +0530

    Time: 456 ms (5.29%), Space: 25.4 MB (5.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,67 @@
 class Solution {
+private:
+    int* count(int* x) {
+        int* c = new int(0);
+        int num = *x;
+        while(num != 0) {
+            num /= 10;
+            ++*c;
+        }
+        return c;
+    }
+
+    int* getDivider(int* index) {
+        int* d = new int(1);
+        int* i = index;
+        while(*i > 1) {
+            *d *= 10;
+            --*i;
+        }
+        return d;
+    }
+
+    int* getNum(int* num, int* index) {
+        int* val = new int(*num);
+        int* idx = new int(*index);
+        int* d = getDivider(idx);
+        *val /= *d;
+        *val %= 10;
+        delete d;
+        delete idx;
+        return val;
+    }
+
+    bool check(int* x) {
+        int* c = count(x);
+        int* i = new int(1);
+        int* j = new int(*c);
+        while(*j > *i) {
+            int* left = getNum(x, j);
+            int* right = getNum(x, i);
+            cout << "left:" << *left << ", right:" << *right
+            << ", c:" << *c << "[END]";
+            if(*left != *right) {
+                delete c;
+                delete i;
+                delete j;
+                delete left;
+                delete right;
+                return false;
+            }
+            delete left;
+            delete right;
+            ++*i;
+            --*j;
+        }
+        delete c;
+        delete i;
+        delete j;
+        return true;
+    }
+
 public:
     bool isPalindrome(int x) {
-        return isPalindrome(0, to_string(x).length()-1, to_string(x));
-    }
-    bool isPalindrome(int i, int r, string str) {
-        if(i>r) {
-            return true;
-        } else if(str[i] != str[r]) {
-            return false;
-        } else {
-            return isPalindrome(i+1, r-1, str);
-        }
+        if(x < 0) return false;
+        return check(&x);
     }
 };
