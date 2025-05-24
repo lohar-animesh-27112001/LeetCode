c9a83fc050132bb34e7ef42f4aeaecaa1bc41d2f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-05-24 12:09:47 +0530

    Time: 2 ms (75.96%), Space: 7.9 MB (22.66%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -10,10 +10,10 @@ private:
     }
 public:
     int fib(int n) {
-        vector<int> memory(n+1, -1);
-        return fib(n, memory);
+        // vector<int> memory(n+1, -1);
+        // return fib(n, memory);
         // Tablulation Approach
-        // if(n == 0 || n == 1) return n;
+        if(n == 0 || n == 1) return n;
         int first = 0;
         int second = 1;
         for(int i = 2; i <= n; i++) {
