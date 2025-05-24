c237bb79fba3b5dd5d71fb5f6fa76fa5819f388f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-05-24 12:09:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (5.06%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int fib(int n, vector<int> &dp) {
+    int fib(int n, vector<int> &dp) { // DP + Recursiom
         if(n == 0 || n == 1) {
             dp[n] = n;
             return n;
@@ -12,5 +12,15 @@ public:
     int fib(int n) {
         vector<int> memory(n+1, -1);
         return fib(n, memory);
+        // Tablulation Approach
+        // if(n == 0 || n == 1) return n;
+        int first = 0;
+        int second = 1;
+        for(int i = 2; i <= n; i++) {
+            int num = first + second;
+            first = second;
+            second = num;
+        }
+        return second;
     }
 };
