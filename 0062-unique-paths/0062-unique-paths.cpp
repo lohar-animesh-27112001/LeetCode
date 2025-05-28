46ba0f380e39283e4b73f28af53798d5cb6e4d50
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-05-28 14:47:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void uniquePaths(int &m, int &n, int &ans, int i, int j,
+    void uniquePaths(int &m, int &n, int i, int j,
                     vector<vector<int>> &dp) {
         if(m-1 == i && n-1 == j) {
             dp[i][j] = 1;
@@ -8,16 +8,15 @@ private:
         }
         if(i>=m || j>=n) return;
         if(dp[i][j] != -1) return;
-        uniquePaths(m,n,ans,i+1,j,dp);
-        uniquePaths(m,n,ans,i,j+1,dp);
+        uniquePaths(m,n,i+1,j,dp);
+        uniquePaths(m,n,i,j+1,dp);
         dp[i][j] = ((i+1<m) ? dp[i+1][j] : 0) + 
                    ((j+1<n) ? dp[i][j+1] : 0);
     }
 public:
     int uniquePaths(int m, int n) {
-        int uPaths = 0;
         vector<vector<int>> mem(m, vector<int>(n,-1));
-        uniquePaths(m,n,uPaths,0,0,mem);
+        uniquePaths(m,n,0,0,mem);
         return mem[0][0];
     }
 };
