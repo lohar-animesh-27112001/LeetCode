efa125d4c1d222d7945dc507b5e5dfacca5590ba
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-07-20 13:15:03 +0530

    Time: 0 ms (100%), Space: 97.6 MB (7.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,4 +1,23 @@
 class Solution {
+private:
+    int maxProf(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 0) return 0;
+        int buy = prices[0];
+        int profit = 0;
+        for(int i = 1; i < n; i++) {
+            if(prices[i] >= prices[i - 1])
+                continue;
+            else {
+                profit += prices[i - 1] - buy;
+                buy = prices[i];
+            }
+            cout << profit << " ";
+        }
+        if(prices[n - 1] > buy)
+            profit += prices[n - 1] - buy;
+        return profit;
+    }
 public:
     int maxProfit(vector<int>& prices) {
         int sell = 0;
