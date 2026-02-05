1cdf4914037b0a1543b5e4d0029c80189488856f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2026-02-05 19:26:48 +0530

    Time: 7 ms (42.79%), Space: 16.1 MB (5.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v = nums;
-        sort(v.begin(), v.end());
-        
-        int r = v.size() - 1;
-        int i = 0;
-        
-        while (i < r) {
-            if (v[i] + v[r] > target) {
-                r--;
-            } else if (v[i] + v[r] < target) {
-                i++;
-            } else {
-                int first = -1, second = -1;
-                for (int k = 0; k < nums.size(); k++) {
-                    if (nums[k] == v[i]) {
-                        first = k;
-                        break;
-                    }
-                }
-                for (int k = 0; k < nums.size(); k++) {
-                    if (nums[k] == v[r] && k != first) {
-                        second = k;
-                        break;
-                    }
-                }
-                return {first, second};
-            }
+        unordered_map<int, int> um;
+        int a = 0;
+        for(int num : nums)
+            um.insert({target - num, a++});
+        a = 0;
+        for(int num : nums) {
+            auto i = um.find(num);
+            if(i != um.end() && i->second != a)
+                return {a, i->second};
+            a++;
         }
         return {};
-    }
-    vector<int> twoSum2(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++) {
-            for(int j=i+1; j<nums.size(); j++) {
-                if((nums[i] + nums[j]) == target) {
-                    vector<int> v = {i,j};
-                    return v;
-                }
-            }
-        }
-        vector<int> v;
-        return v;
     }
 };
