c177533e026da30bcb52a10df0bb7cf9f18f6c80
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2026-07-25 18:44:14 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,44 +1,20 @@
 class Solution {
+private:
+    void sort012(vector<int> &arr, int n) {
+        int lo = 0, mid = 0, hi = n-1;
+        while(mid <= hi) {
+            if(arr[mid] == 0) {
+                swap(arr[lo++], arr[mid++]);
+            } else if(arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr[mid], arr[hi--]);
+            }
+        }
+    }
 public:
     void sortColors(vector<int>& nums) {
-        count_sort_all(nums);
-    }
-private:
-    void count_sort(vector<int> &nums) {
-        int max = 2;
-        vector<int> countArray(max + 1, 0);
-        
-        for(int i : nums) {
-            countArray[i]++;
-        }
-
-        int index = 0;
-        for(int i = 0; i <= max; i++) {
-            while(countArray[i] > 0) {
-                nums[index++] = i;
-                countArray[i]--;
-            }
-        }
-    }
-    void count_sort_all(vector<int> &nums) {
-        int max = 0;
-        for(int i : nums) {
-            if(i > max) {
-                max = i;
-            }
-        }
-        vector<int> countArray(max + 1, 0);
-        for(int i : nums) {
-            countArray[i] ++;
-        }
-        for(int i = 1; i <= max; i++) {
-            countArray[i] += countArray[i - 1];
-        }
-        vector<int> outputArray(nums.size());
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            outputArray[countArray[nums[i]] - 1] = nums[i];
-            countArray[nums[i]] --;
-        }
-        nums = outputArray;
+        int n = nums.size();
+        sort012(nums, n);
     }
 };
