35b4fb12e7298fc081df50193ec98fc03bdab529
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2026-07-26 18:00:46 +0530

    Time: 0 ms (100%), Space: 26.2 MB (5.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,26 @@
 class Solution {
+private:
+    vector<int> sortingSegregateElements(vector<int>& arr, int n) {
+        vector<int> count_min;
+        vector<int> count_max;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 0) count_min.push_back(i);
+            else count_max.push_back(i);
+        }
+        int min = count_min.size();
+        int max = count_max.size();
+        vector<int> ans;
+        for(int i = 0; i < max; i++) {
+            ans.push_back(arr[count_max[i]]);
+        }
+        for(int i = 0; i < min; i++) {
+            ans.push_back(arr[count_min[i]]);
+        }
+        return ans;
+    }
 public:
     void moveZeroes(vector<int>& nums) {
-        int first = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != 0) {
-                nums[first++] = nums[i];
-            }
-        }
-        for(int i = first; i < nums.size(); i++) {
-            nums[i] = 0;
-        }
+        int n = nums.size();
+        nums = sortingSegregateElements(nums, n);
     }
 };
