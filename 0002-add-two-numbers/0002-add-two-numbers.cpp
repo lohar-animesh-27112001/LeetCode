2e02fe44051ec6dd925fbab514703d6a9da0a98f
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2026-08-29 20:18:35 +0530

    Time: 3 ms (34.04%), Space: 78.2 MB (12.8%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -80,3 +80,67 @@ public:
         return ans_listnode;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         vector<int> arr1 = {};
+//         vector<int> arr2 = {};
+//         int num1 = 0;
+//         int num2 = 0;
+//         ListNode* temp1 = l1;
+//         ListNode* temp2 = l2;
+//         while(temp1 != NULL) {
+//             arr1.insert(arr1.begin(), temp1->val);
+//             temp1 = temp1->next;
+//         }
+//         while(temp2 != NULL) {
+//             arr2.insert(arr2.begin(), temp2->val);
+//             temp2 = temp2->next;
+//         }
+//         int n = arr1.size();
+//         int m = arr2.size();
+//         for(int i = 0; i < n; i++) {
+//             int temp = pow(10, i);
+//             num1 = num1 + temp * arr1[n-i-1];
+//         }
+//         for(int i = 0; i < m; i++) {
+//           }   long temp = pow(10, i);
+//             num2 = num2 + temp * arr2[m-i-1];
+       
+//         int ans = num1 + num2;
+//         ListNode* l3;
+//         ListNode* ans_listnode = NULL;
+//         bool first_time = true;
+//         while(ans != 0) {
+//             ListNode* temp = new ListNode();
+//             temp->val = ans % 10;
+//             temp->next = NULL;
+//             if(first_time == true) {
+//                 l3 = temp;
+//                 ans_listnode = l3;
+//                 first_time = false;
+//             } else {
+//                 l3->next = temp;
+//                 l3 = l3->next;
+//             }
+//             ans = ans / 10;
+//         }
+//         if(ans_listnode == NULL) {
+//             ans_listnode = new ListNode();
+//             ans_listnode->val = 0;
+//             ans_listnode->next = NULL;
+//         }
+//         return ans_listnode;
+//     }
+// };
